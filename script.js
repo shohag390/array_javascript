1fe0b9ae14336b7f62d091dfe67b405ebc337db5
shohag390 <shohag390@users.noreply.github.com>
2025-02-08 12:39:15 +0600

    Task 1 complete

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
-const myArray = ["shohag", "bappy", "mowsumi", "sidratul"]
-console.log(myArray);
+// const myArray = ["shohag", "bappy", "mowsumi", "sidratul"]
+// console.log(myArray);
 
-console.log(myArray.length);
+// console.log(myArray.length);
 
-console.log(myArray[3]);
+// console.log(myArray[3]);
+
+
+// let x = "10";
+// let num = Number(x);
+// console.log(num);
+
+// let y = "10.99"
+// let num2 = parseInt(y);
+// console.log(num2);
+
+// let z = "10.99";
+// let num3 = parseFloat(z);
+// console.log(num3);
+
+
+// var y;
+// var x = 10;
+// var reult = x + y;
+// // var num = x.toFixed(2);
+// // console.log(x.toFixed(1));
+// console.log(reult);
+
+// console.log(isNaN(reult));
+
+
+// var x = 10
+
+// x += 10
+
+// console.log(x);
+
+
+// task 1 
+var iHaveMoney = 1000;
+var appleAndOrange = 700;
+var result = iHaveMoney - appleAndOrange;
+console.log(result);
+
