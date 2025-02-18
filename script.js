5b387d2363902553b3f77d284dce9c57fd0b3ef7
shohag390 <shohag390@users.noreply.github.com>
2025-02-18 22:54:39 +0600

    Total conseft add

--- script.js
+++ script.js
@@ -36,9 +36,75 @@
 // console.log(x);
 
 
-// task 1 
-var iHaveMoney = 1000;
-var appleAndOrange = 700;
-var result = iHaveMoney - appleAndOrange;
-console.log(result);
+// task 1
+// var iHaveMoney = 1000;
+// var appleAndOrange = 700;
+// var result = iHaveMoney - appleAndOrange;
+// console.log(result);
 
+// Task 2
+// var math = 75.25;
+// var biology = 65;
+// var chemistry = 80;
+// var physics = 35.45;
+// var bangla = 99.50;
+
+// var result2 = (math + biology + chemistry + physics + bangla) / 5
+
+// console.log(result2.toFixed(2));
+
+
+// let age = 12;
+// let withAdult = true
+
+// if (age >= 15) {
+//     console.log("apni malai dhukte parben");
+//     if (withAdult) {
+//         console.log("apni voutik bari jate parben");
+//     } else {
+//         console.log("apni voutik bari jate parben na");
+
+//     }
+
+// } else {
+//     console.log("apnai malai dhukte parben na");
+
+// }
+
+
+
+// const mobile = [
+//     {
+//         name: "samsung",
+//         price: 12000
+//     },
+//     {
+//         name: "itel",
+//         price: 9000
+//     },
+//     {
+//         name: "Iphone 11",
+//         price: 7000
+//     },
+//     {
+//         name: "samphone",
+//         price: 8000
+//     },
+// ]
+
+// function cheepPhone(phones) {
+
+//     let min = phones[0]
+
+//     for (const phone of phones) {
+//         if (phone.price < min.price) {
+//             min = phone
+//         }
+//     }
+
+//     return min;
+
+// }
+
+// const choose = cheepPhone(mobile)
+// console.log(choose);
